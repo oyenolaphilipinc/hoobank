6f8b546445128f1743c02b81b7e58f5582712965
Philip <oyenolaphilip89@gmail.com>
2024-02-24 23:06:25 +0000

    Hello Virtiai

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -22,7 +22,7 @@ const Business = () =>  (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>
-        You do the comanion, <br className="sm:block hidden" /> our model does
+        You do the companion, <br className="sm:block hidden" /> our model does
         the magic.
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
